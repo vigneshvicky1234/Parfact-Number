8c9b28c4fb42a9084b8038931581da7ff4f7ade7
vigneshvicky1234 <127626606+vigneshvicky1234@users.noreply.github.com>
2023-03-13 06:34:49 +0530

    Add files via upload

---
+++ Perfact Number.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = 6;
+int sum = 0;
+for (int i = 1; i <n; i++)
+{
+    if (n%i== 0)
+    {
+        sum = sum + i;
+
+
+    }
+}
+    if (n == sum)
+    {
+        Console.WriteLine("This is perfact number " );
+
+    }
+    else
+    {
+        Console.WriteLine(" Your Number is Not Perfact Number");
+    }
+
+
+
+
